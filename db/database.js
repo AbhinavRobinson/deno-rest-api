eaa03358fe1956a7b47f19b692064dfd62771d39
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-09-14 12:37:06 +0530

    🚚(feat)#3: Added DB connector and schema
    Added DB connector and db schema and db config schema.

---
+++ db/database.js
@@ -0,0 +1,48 @@
+/**
+ * ? About
+ * * import postgres client and connects to database.
+ */
+import { Client } from "https://deno.land/x/postgres/mod.ts";
+
+/**
+ * ? About
+ * * import postgres server config
+ */
+const cfg = require('./config.json').db
+
+/**
+ * ! Connection Constructor Class
+ */
+class Database {
+  
+  /**
+   * ? About
+   * * Calls Connection Promise
+   */
+  constructor() {
+    this.connect();
+  }
+  
+  /**
+   * ! Connection
+   * * connects client to postgres database.
+   */
+
+  async connect() {
+   this.client = new Client({
+      user: cfg.user,
+      database: cfg.database,
+      hostname: cfg.hostname,
+      password: cfg.password,
+      port: cfg.port
+    });
+
+    await this.client.connect();
+  }
+}
+
+/**
+ * ? About
+ * * Exports Construction Class to local enviroment
+ */
+export default new Database().client;
